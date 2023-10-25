920dc266699f3e5ffa2d00c500f3138c3e6c93a9
stanly3099 <stanlyruth@gmail.com>
2023-10-25 18:12:21 +0530

    classA add for the first time as master

---
+++ src/test/java/com/gitclass/ClassA.java
@@ -0,0 +1,16 @@
+package com.gitclass;
+
+public class ClassA {
+private void method1() {
+ System.out.println("1");
+
+}
+private void mehtod2() {
+	 System.out.println("2");
+
+}
+private void method3() {
+	 System.out.println("3");
+
+}
+}
