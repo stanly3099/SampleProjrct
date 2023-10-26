b805cb5d3741a29304b70dd7503c471c2e92c28a
karyo123 <sakaryo10@gmail.com>
2023-10-26 09:28:51 +0530

    mathods added

--- src/test/java/com/gitclass/ClassA.java
+++ src/test/java/com/gitclass/ClassA.java
@@ -25,5 +25,14 @@ private void method5() {
 private void method6() {
 	System.out.println("6");
 
+
+}
+private void method7() {
+	System.out.println("7");
+
+}
+private void method8() {
+	System.out.println("8");
+
 }
 }
